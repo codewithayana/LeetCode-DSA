c0c6a212c530fa27c4e09a4aeca76550800f60b3
Ayana Dinesh <codewithayana@gmail.com>
2025-11-22 19:40:22 +0530

    Time: 53 ms (14.25%), Space: 54.1 MB (39.92%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -3,15 +3,14 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-    
     return function() {
-       return n++; 
+        return n++; 
     };
 };
 
 /** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
+ * const counter = createCounter(10);
+ * counter(); // 10
+ * counter(); // 11
+ * counter(); // 12
  */
