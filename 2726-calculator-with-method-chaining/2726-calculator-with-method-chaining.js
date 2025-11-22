367da66c06fec9751dbf32e0258a511fb3d06cb5
Ayana Dinesh <codewithayana@gmail.com>
2025-11-22 19:33:53 +0530

    Time: 49 ms (34.54%), Space: 54.2 MB (34.98%) - LeetHub

---
+++ 2726-calculator-with-method-chaining/2726-calculator-with-method-chaining.js
@@ -0,0 +1,66 @@
+class Calculator {
+    /** 
+     * @param {number} value
+     */
+    constructor(value) {
+        this.value = value;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    add(value) {
+        this.value += value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    subtract(value) {
+        this.value -= value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */  
+    multiply(value) {
+        this.value *= value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator|string}
+     */
+    divide(value) {
+        if (value === 0) {
+            this.value = null; 
+            this.error = "Division by zero is not allowed";
+        } else {
+            this.value /= value;
+        }
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    power(value) {
+        this.value **= value;
+        return this;
+    }
+    
+    /** 
+     * @return {number|string}
+     */
+    getResult() {
+        if (this.value === null) return this.error;
+        return this.value;
+    }
+}
