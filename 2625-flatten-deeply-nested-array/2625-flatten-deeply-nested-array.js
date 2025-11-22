b23af277d726307c6db68ff04eb2c306c6001a0f
Ayana Dinesh <codewithayana@gmail.com>
2025-11-22 19:17:26 +0530

    Time: 167 ms (20.62%), Space: 91.6 MB (42.78%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,15 @@
+var flat = function(arr, n) {
+    if (n === 0) return arr;
+
+    let result = [];
+
+    for (let item of arr) {
+        if (Array.isArray(item)) {
+            result.push(...flat(item, n - 1)); 
+        } else {
+            result.push(item);
+        }
+    }
+
+    return result;
+};
