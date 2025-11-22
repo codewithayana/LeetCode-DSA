612a564f59a665998a99f7fb30ee40a2ebfa03bb
Ayana Dinesh <codewithayana@gmail.com>
2025-11-22 18:53:30 +0530

    Time: 1 ms (78.95%), Space: 55.4 MB (75.09%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    const words = s.trim().split(/\s+/);
+    words.reverse()
+
+    return words.join(" ");
+};
