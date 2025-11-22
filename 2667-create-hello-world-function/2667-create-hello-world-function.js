fc114a7fa72fb67e1e8ce06a245501c34bff8a7d
Ayana Dinesh <codewithayana@gmail.com>
2025-11-22 19:38:06 +0530

    Time: 45 ms (54.59%), Space: 53.1 MB (92.69%) - LeetHub

--- 2667-create-hello-world-function/2667-create-hello-world-function.js
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -2,9 +2,8 @@
  * @return {Function}
  */
 var createHelloWorld = function() {
-    
-    return function(...args) {
-        return "Hello World"
+    return function() {
+        return "Hello World";
     }
 };
 
