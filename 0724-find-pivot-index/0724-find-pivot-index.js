3a2265e1322b3b9fad3c54a8545622b71f7c4ebd
Ayana Dinesh <codewithayana@gmail.com>
2025-11-29 18:57:30 +0530

    Time: 1 ms (90.19%), Space: 55.9 MB (91.32%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let leftSum = 0;
+    let total = nums.reduce((a, b) => a + b, 0);
+
+    for (let i = 0; i < nums.length; i++) {
+        let rightSum = total - leftSum - nums[i];
+
+        if (leftSum === rightSum) {
+            return i;
+        }
+
+        leftSum += nums[i];
+    }
+
+    return -1;
+};
