60ec475f38876c93b8f2130b3e55daf7747c6acc
Ayana Dinesh <codewithayana@gmail.com>
2025-12-11 22:30:37 +0530

    Time: 1 ms (79.45%), Space: 56.6 MB (50.11%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function(rooms) {
+   const visited = new Set();
+    
+    function dfs(room) {
+        if (visited.has(room)) return;
+        visited.add(room);
+        for (const key of rooms[room]) {
+            dfs(key);
+        }
+    }
+    
+    dfs(0); 
+    return visited.size === rooms.length; 
+};
