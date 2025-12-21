c3fea320243cdde0c374fbd2eafc30e5b4795772
Ayana Dinesh <codewithayana@gmail.com>
2025-12-21 23:16:18 +0530

    Time: 26 ms (87.32%), Space: 57.1 MB (88.17%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(text1, text2) {
+     const m = text1.length;
+    const n = text2.length;
+
+    let prev = new Array(n + 1).fill(0);
+    let curr = new Array(n + 1).fill(0);
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (text1[i - 1] === text2[j - 1]) {
+                curr[j] = 1 + prev[j - 1];
+            } else {
+                curr[j] = Math.max(prev[j], curr[j - 1]);
+            }
+        }
+        [prev, curr] = [curr, prev];
+    }
+
+    return prev[n];
+};
