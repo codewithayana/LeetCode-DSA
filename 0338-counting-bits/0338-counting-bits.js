872938ae1011b54a746290051dc8216aea0cfd93
Ayana Dinesh <codewithayana@gmail.com>
2025-12-23 22:17:27 +0530

    Time: 2 ms (69.82%), Space: 60.6 MB (92.98%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function(n) {
+    const res = new Array(n + 1).fill(0);
+    
+    for (let i = 1; i <= n; i++) {
+        res[i] = res[i >> 1] + (i & 1);
+    }
+    
+    return res;
+};
