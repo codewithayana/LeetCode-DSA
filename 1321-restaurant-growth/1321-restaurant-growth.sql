42a004fa0acbde2295ab12d172f625d4913727a3
Ayana Dinesh <codewithayana@gmail.com>
2026-02-01 21:44:04 +0530

    Time: 391 ms (17.26%), Space: 0B (100%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,24 @@
+# Write your MySQL query statement below
+SELECT
+    visited_on,
+    SUM(daily_amount) OVER (
+        ORDER BY visited_on
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS amount,
+    ROUND(
+        AVG(daily_amount) OVER (
+            ORDER BY visited_on
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ), 2
+    ) AS average_amount
+FROM (
+    SELECT
+        visited_on,
+        SUM(amount) AS daily_amount
+    FROM Customer
+    GROUP BY visited_on
+) d
+ORDER BY visited_on
+LIMIT 1000000 OFFSET 6;
+
+    
