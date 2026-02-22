587290dda1d08648bb0dad805a85fc991040a4a1
Ayana Dinesh <codewithayana@gmail.com>
2026-02-22 22:08:24 +0530

    Time: 0 ms (100%), Space: 54.3 MB (36.11%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var binaryGap = function(n) {
+    let last = -1;
+    let maxGap = 0;
+    let index = 0;
+
+    while (n > 0) {
+      if(n & 1) {
+        if (last !== -1) {
+            maxGap = Math.max(maxGap, index -last);       
+      }
+      last = index;
+    }
+    n >>= 1;
+    index++;
+     }
+     return maxGap;
+};
