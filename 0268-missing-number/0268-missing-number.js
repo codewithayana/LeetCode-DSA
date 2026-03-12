f265127417b555c0adfe3cae52e67b96a5fa48fe
Ayana Dinesh <codewithayana@gmail.com>
2026-03-12 20:55:23 +0530

    Time: 3 ms (43.21%), Space: 57.3 MB (41.82%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let n = nums.length;
+    let expectedSum = n* (n+1) /2;
+    
+    let actualSum =0;
+    for (let num of nums) {
+        actualSum += num;
+    }
+    return expectedSum - actualSum
+};
